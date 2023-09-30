460f37a996e0df76548e7f8b44a2244b689d99dd
yacervantes <ycervantes2221@gmail.com>
2023-09-30 11:29:44 -0500

    Initial Commit

---
+++ ReGitHubDemo/ReGitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReGitHubDemoApp.swift
+//  ReGitHubDemo
+//
+//  Created by student on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ReGitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
